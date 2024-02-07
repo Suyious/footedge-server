765263242a72ba88eaaf17753fd3081a04b3d77a
Suyash K. <suyu162001@gmail.com>
2024-02-07 20:01:20 +0530

    feat: server setup

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const io = new Server(PORT, {
     cors: true
 });
 
+const emailToSocket = new Map();
+const socketToEmail = new Map();
+
 io.on("connection", (socket) => {
     console.log("Socket Connected: " + socket.id);
+    socket.on("room:join", ({ room }) => {
+        console.log(`User ${socket.id} entered room ${room}`);
+        io.to(room).emit("user:joined", { user: socket.id });
+        socket.join(room);
+        io.to(socket.id).emit("room:join:success", { room })
+    })
 })
