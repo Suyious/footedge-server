bec86e1bb303b6fa58188c382f8373b5431d36b1
Suyash K. <suyu162001@gmail.com>
2024-02-08 22:32:06 +0530

    feat: minor change in index.js

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ const io = new Server(PORT, {
     cors: true
 });
 
-const emailToSocket = new Map();
-const socketToEmail = new Map();
+const UsernameToSocket = new Map();
+const socketToUsername = new Map();
 
+// On entering browser
 io.on("connection", (socket) => {
     console.log("Socket Connected: " + socket.id);
-    socket.on("room:join", ({ room }) => {
-        console.log(`User ${socket.id} entered room ${room}`);
-        io.to(room).emit("user:joined", { user: socket.id });
-        socket.join(room);
+    // on clicking join in /lobby/:id
+    socket.on("room:join", ({ room, username }) => {
+        // to navigate to /room/:id
         io.to(socket.id).emit("room:join:success", { room })
+        // to notify other users in the room
+        io.to(room).emit("user:joined", { user: socket.id, username });
+        // to actually join the room
+        socket.join(room);
+
+        console.log(`User ${socket.id}: ${username} entered room ${room}`);
     })
 })
